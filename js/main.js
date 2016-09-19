3c83e95247d1aecf3017bac11f25d83b23304fb5
Barry Walsh <barryswalsh89@gmail.com>
2016-09-19 00:11:05 -0700

    put all our JS into the namespace skewfo. This is so we do not need to use any global variables. for example difference is now changed using getters and setters. May need some work to improve on this, as I have not really namspaced stuff before.

--- js/main.js
+++ js/main.js
@@ -1,94 +1,108 @@
-	var difference = 0;
-	function alterSkew() {
-				sxVal  = document.getElementById("sx").value;
-				syVal  = document.getElementById("sy").value;
-				document.getElementById("i1").value = sxVal;
-				document.getElementById("i2").value = syVal;
+var skewfo = {
+	difference : {
+		value: 0,
+		getValue: function(){
+			return this.value;
+		},
+		setValue: function(setValue){
+			this.value = setValue;
+		}
+	},
+	bxVal :{
+		value: 0,
+		getValue: function(){
+			return this.value;
+		},
+		setValue: function(setValue){
+			this.value = setValue;
+		}
+	},
+	alterSkew: function(){
+		var sxVal, syVal, byVal, pxVal, rxVal, ryVal, rzVal, mxVal, myVal;
+		sxVal  = document.getElementById("sx").value;
+		syVal  = document.getElementById("sy").value;
+		document.getElementById("i1").value = sxVal;
+		document.getElementById("i2").value = syVal;
 
-				// SCALE
-				// Do Not Preserve Aspect Ratio
-				if(!document.getElementById("aspectT").checked){
-					document.getElementById("by").disabled = false;
-					bxVal  = document.getElementById("bx").value/100;
-					byVal  = document.getElementById("by").value/100;
-					document.getElementById("i3").value = bxVal;
-					document.getElementById("i4").value = byVal;
-				}
-				// Preserve Aspect Ratio
-				else {
-					document.getElementById("by").disabled = true;
+		// SCALE
+		// Do Not Preserve Aspect Ratio
+		if(!document.getElementById("aspectT").checked){
+			document.getElementById("by").disabled = false;
+			this.bxVal.setValue(document.getElementById("bx").value/100);
+			byVal  = document.getElementById("by").value/100;
+			document.getElementById("i3").value = this.bxVal.getValue();
+			document.getElementById("i4").value = byVal;
+		}
+		// Preserve Aspect Ratio
+		else {
+			document.getElementById("by").disabled = true;
 
-					bxVal  = document.getElementById("bx").value/100;
-										byVal  = bxVal - difference;
+			this.bxVal.setValue(document.getElementById("bx").value/100);
+								byVal  = this.bxVal.getValue() - this.difference.getValue();
 
-					document.getElementById("i3").value = bxVal;
-					document.getElementById("i4").value = byVal;
-				}
+			document.getElementById("i3").value = this.bxVal.getValue();
+			document.getElementById("i4").value = byVal;
+		}
 
-				// PERSPECTIVE
-				pxVal  = document.getElementById("px").value;
-				document.getElementById("i5").value = pxVal;
+		// PERSPECTIVE
+		pxVal  = document.getElementById("px").value;
+		document.getElementById("i5").value = pxVal;
 
-				// ROTATION
-				rxVal  = document.getElementById("rx").value;
-				ryVal  = document.getElementById("ry").value;
-				rzVal  = document.getElementById("rz").value;
-				document.getElementById("i7").value = rxVal;
-				document.getElementById("i8").value = ryVal;
-				document.getElementById("i11").value = rzVal;
+		// ROTATION
+		rxVal  = document.getElementById("rx").value;
+		ryVal  = document.getElementById("ry").value;
+		rzVal  = document.getElementById("rz").value;
+		document.getElementById("i7").value = rxVal;
+		document.getElementById("i8").value = ryVal;
+		document.getElementById("i11").value = rzVal;
 
-				// Translation MOVE
-				mxVal  = document.getElementById("mx").value;
-				myVal  = document.getElementById("my").value * -1;
-				document.getElementById("i9").value = mxVal;
-				document.getElementById("i10").value = myVal;
+		// Translation MOVE
+		mxVal  = document.getElementById("mx").value;
+		myVal  = document.getElementById("my").value * -1;
+		document.getElementById("i9").value = mxVal;
+		document.getElementById("i10").value = myVal;
 
-				
-				document.getElementById("iframeWrapper").style.transform = "skew(" + sxVal + "deg, " + syVal + "deg) scale(" + bxVal + ", " + byVal + ")" + " perspective(" + pxVal + "px)  rotateY(" + ryVal + "deg) rotateX(" + rxVal + "deg) rotateZ(" + rzVal + "deg) translateX("+ mxVal +"px) translateY("+ myVal +"px)";
-				
-			}
+		
+		document.getElementById("iframeWrapper").style.transform = "skew(" + sxVal + "deg, " + syVal + "deg) scale(" + this.bxVal.getValue() + ", " + byVal + ")" + " perspective(" + pxVal + "px)  rotateY(" + ryVal + "deg) rotateX(" + rxVal + "deg) rotateZ(" + rzVal + "deg) translateX("+ mxVal +"px) translateY("+ myVal +"px)";
+		
+	},
+	setTablet: function() {
+		document.getElementsByClassName("skewFrame")[0].id = "iframe-tablet";
+	},
+	setMobile: function() {
+		document.getElementsByClassName("skewFrame")[0].id = "iframe-mobile";
+	},
+	setDesktop: function(){
+		document.getElementsByClassName("skewFrame")[0].id = "iframe-desktop";
+	},
+	reset: function() {
+		document.getElementById("sx").value = 0;
+		document.getElementById("sy").value = 0;
+		document.getElementById("rx").value = 0;
+		document.getElementById("ry").value = 0;
+		document.getElementById("px").value = 90;
+		document.getElementById("bx").value = 100;
+		document.getElementById("by").value = 100;
+		document.getElementById("mx").value = 0;
+		document.getElementById("my").value = 0;
 
-			function setTablet() {
-				document.getElementsByClassName("skewFrame")[0].id = "iframe-tablet";
-			}
+		this.alterSkew();
+	},
+	aspectRatioChange: function(){
+		this.difference.setValue(this.bxVal.getValue() - document.getElementById("by").value/100); 
+	},
+	inputIframe: function(){
+		var val, iframe;
+			iframe = document.getElementsByClassName("skewFrame")[0];
+			val = document.getElementById('skewInput').value;
+			iframe.src = val;
+	}
 
-			function setMobile() {
-				document.getElementsByClassName("skewFrame")[0].id = "iframe-mobile";
-			}
+};
 
-			function setDesktop() {
-				document.getElementsByClassName("skewFrame")[0].id = "iframe-desktop";
-			}
-
-			function reset() {
-				document.getElementById("sx").value = 0;
-				document.getElementById("sy").value = 0;
-				document.getElementById("rx").value = 0;
-				document.getElementById("ry").value = 0;
-				document.getElementById("px").value = 90;
-				document.getElementById("bx").value = 100;
-				document.getElementById("by").value = 100;
-				document.getElementById("mx").value = 0;
-				document.getElementById("my").value = 0;
-
-				alterSkew();
-			}
-
-			function aspectRatioChange() {
-				difference = bxVal - document.getElementById("by").value/100;
-			}
-
-			function inputIframe(){
-				var val, iframe;
-				iframe = document.getElementsByClassName("skewFrame")[0];
-				val = document.getElementById('skewInput').value;
-				iframe.src = val;
-			}
-
-
-			// Slider
-			jQuery(document).ready(function($) {
-				$('.my-slider').unslider({
-					
-				});
-			});
+	// Slider
+	jQuery(document).ready(function($) {
+		$('.my-slider').unslider({
+			
+		});
+	});
